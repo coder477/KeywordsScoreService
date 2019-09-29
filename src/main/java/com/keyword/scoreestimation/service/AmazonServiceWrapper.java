9ced5b4acc4b8a154c4b7f5dc70eec230ac8948e
anirudh <anirudh.wulligundam@n26.com>
2019-09-29 12:35:57 +0200

    pipe everything

--- src/main/java/com/keyword/scoreestimation/service/AmazonServiceWrapper.java
+++ src/main/java/com/keyword/scoreestimation/service/AmazonServiceWrapper.java
@@ -3,6 +3,8 @@ package com.keyword.scoreestimation.service;
 import com.keyword.scoreestimation.controller.KeywordSearchController;
 import com.keyword.scoreestimation.exception.BadRequestException;
 import com.keyword.scoreestimation.exception.ExternalApiException;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -26,14 +31,18 @@ public class AmazonServiceWrapper {
 
 	private final String AUTO_COMPLETE_API = "https://completion.amazon.com/search/complete?method=completion&mkt=1&search-alias=aps&q=";
 
-	public List<String> getKeyWordData(String index) {
+	public Pair<String, List<String>> getKeyWordData(String index) {
 		try {
+
 			log.info("Fetching keywords for prefix " + index);
+
 			JSONArray responseArray = new JSONArray(
 					restTemplate.getForObject(AUTO_COMPLETE_API.concat(index), String.class)
 			).getJSONArray(1);
-			return IntStream.range(0, responseArray.length())
+
+			List<String> amazonSearchWords = IntStream.range(0, responseArray.length())
 					.mapToObj(responseArray::getString).collect(Collectors.toList());
+			new ImmutablePair<>(index, amazonSearchWords);
 		} catch (HttpStatusCodeException httpException) {
 			if (httpException.getStatusCode().is4xxClientError()) {
 				throw new BadRequestException(httpException.getStatusCode(), httpException.getResponseBodyAsString());
--- src/main/java/com/keyword/scoreestimation/service/KeywordSearchService.java
+++ src/main/java/com/keyword/scoreestimation/service/KeywordSearchService.java
@@ -1,6 +1,7 @@
 package com.keyword.scoreestimation.service;
 
 import com.keyword.scoreestimation.response.KeyWordScoreResponse;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +9,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 public class KeywordSearchService {
@@ -26,6 +29,33 @@ public class KeywordSearchService {
 		return new KeyWordScoreResponse(keyword, getScore(keyword));
 	}
 
+	private Double getScoreForKeyWord(String keyword) throws InterruptedException {
+		List<String> prefixes = getPrefixesArray(keyword);
+
+		List<Callable<Pair<String, List<String>>>> callables = prefixes
+				.stream()
+				.<Callable<Pair<String, List<String>>>>map(
+						p -> () -> amazonServiceWrapper.getKeyWordData(p)
+				).collect(Collectors.toList());
+
+		List<Future<Pair<String, List<String>>>> futures = service
+				.invokeAll(callables, 10, TimeUnit.SECONDS);
+
+		double numerator = futures.stream()
+				.map(f -> {
+					try {
+						return f.get();
+					} catch (Exception e) {
+						System.out.println(e);
+						return null;
+					}
+				})
+				.filter(Objects::nonNull)
+				.flatMapToDouble(x -> x.getRight().stream().mapToDouble(y -> getPrefixSum(y, x.getKey(), keyword)))
+				.sum();
+		double denominator = 5.0D * ((keyword.length() * keyword.length()) + keyword.length());
+		return numerator * 100 / denominator;
+	}
 	private Double getScore(String keyword) throws InterruptedException{
 		double finalScore = 0;
 		List<String> prefixes = getPrefixesArray(keyword);
@@ -46,6 +76,9 @@ public class KeywordSearchService {
 			} catch (Exception e) {
 				// need good exception handling
 				System.out.println(e.getMessage());
+			}
+			if (words == null) {
+				continue;
 			}
 			Double prefixSum = 0.0;
 			for (int j = 0; j < words.size(); j++) {
